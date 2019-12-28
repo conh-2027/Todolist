b5dcf8029c3a8eb6f9a63b607f683241ad452326
haunguyen97 <nguyen.cong.hau@sun-asterisk.com>
2019-12-28 22:38:37 +0700

    Them status task

--- application.js
+++ application.js
@@ -1,29 +1,46 @@
 var dr = dragula({})
 
 dr.on('drag', (evt) => {
-  evt.classList.add('rotateOnDrag');
-  console.log("drag");
+    evt.classList.add('rotateOnDrag');
+    console.log("drag");
 });
 
 dr.on('dragend', (evt) => {
-  evt.classList.remove('rotateOnDrag');
-  console.log("dragend");
+    evt.classList.remove('rotateOnDrag');
+    console.log("dragend");
 });
 
-$('.project-task-container')
-  .toArray()
-  .forEach(function(v){
-    dr.containers.push(v)
-  })
+dr.on('drop', (el, target) => {
+    let statusEl = el.children[1];
 
-$('.tiny-editor').change(function(){
-  // const dom = $('.tiny-editor')
-  // let val = dom.val()
-  // dom.val(markdown.toHTML(val))
+    if (target.id === "doing-task") {
+        statusEl.classList.remove("badge-success");
+        statusEl.classList.add("badge-warning");
+        statusEl.innerHTML = "In Process";
+    }
+
+    if (target.id === "todo-task") {
+        statusEl.classList.remove("badge-warning");
+        statusEl.classList.add("badge-success");
+        statusEl.innerHTML = "New";
+    }
+});
+
+
+$('.project-task-container')
+    .toArray()
+    .forEach(function(v) {
+        dr.containers.push(v)
+    })
+
+$('.tiny-editor').change(function() {
+    // const dom = $('.tiny-editor')
+    // let val = dom.val()
+    // dom.val(markdown.toHTML(val))
 })
 
 // var popper = new Popper($('#submit')[0], {
-// 		content: 'submit'	
+// 		content: 'submit'
 // 	}, {
 // 			placement: 'left'
 // 	});
@@ -37,54 +54,58 @@ let listTasks = localStorage.getItem("task") ? JSON.parse(localStorage.getItem("
 
 // create elament and add to list task.
 function createNewTaskElement(task) {
-  return createTaskElement(task);
+    return createTaskElement(task);
 }
 
 function createTaskElement(taskText, taskId = 0) {
-  let projectTask = document.createElement("div");
-  let titleTask = document.createElement("span");
-  let controlContainer = document.createElement("div");
-  let editbutton = document.createElement("div");
-  let deletebutton = document.createElement("div");
-  
-  if(taskId === 0){
-    taskId = Date.now();
+    let projectTask = document.createElement("div");
+    let titleTask = document.createElement("span");
+    let controlContainer = document.createElement("div");
+    let label = document.createElement("div");
+    let editbutton = document.createElement("div");
+    let deletebutton = document.createElement("div");
 
-    listTasks.push({
-      taskId: taskId,
-      text: taskText,
-    });
-    localStorage.setItem('task', JSON.stringify(listTasks));
-  }
+    if (taskId === 0) {
+        taskId = Date.now();
 
-  projectTask.className = "project-task";
-  titleTask.className = "task-title";
-  controlContainer.className = "control-container";
-  editbutton.className = "btn btn-warning btn-sm mr-2 edit-task";
-  deletebutton.className = "btn btn-danger btn-sm delete-task";
-  
-  editbutton.class = "edit";
-  deletebutton.class = "delete";
+        listTasks.push({
+            taskId: taskId,
+            text: taskText,
+        });
+        localStorage.setItem('task', JSON.stringify(listTasks));
+    }
 
-  editbutton.innerHTML = "edit";
-  deletebutton.innerHTML = "delete";
-  titleTask.innerHTML = taskText
-  projectTask.setAttribute("task_id", taskId);
+    projectTask.className = "project-task";
+    titleTask.className = "task-title";
+    controlContainer.className = "control-container";
+    label.className = "badge badge-success text-wrap"
+    editbutton.className = "btn btn-warning btn-sm mr-2 edit-task";
+    deletebutton.className = "btn btn-danger btn-sm delete-task";
 
-  projectTask.appendChild(titleTask);
-  projectTask.appendChild(controlContainer);
-  controlContainer.appendChild(editbutton);
-  controlContainer.appendChild(deletebutton);
+    editbutton.class = "edit";
+    deletebutton.class = "delete";
 
-  return projectTask;
+    label.innerHTML = "New"
+    editbutton.innerHTML = "edit";
+    deletebutton.innerHTML = "delete";
+    titleTask.innerHTML = taskText
+    projectTask.setAttribute("task_id", taskId);
+
+    projectTask.appendChild(titleTask);
+    projectTask.appendChild(label);
+    projectTask.appendChild(controlContainer);
+    controlContainer.appendChild(editbutton);
+    controlContainer.appendChild(deletebutton);
+
+    return projectTask;
 }
 
-let initTask = function(){
-  $.each(listTasks, function (_, value) {
-    console.log(value['text']);
-    let task = createTaskElement(value['text'], value['taskId']);
-    todo.appendChild(task);
-  });
+let initTask = function() {
+    $.each(listTasks, function(_, value) {
+        console.log(value['text']);
+        let task = createTaskElement(value['text'], value['taskId']);
+        todo.appendChild(task);
+    });
 }
 
 initTask();
@@ -92,78 +113,78 @@ delete_task();
 edit_task();
 
 function allowDrop(ev) {
-  ev.preventDefault();
+    ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+    ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-  ev.preventDefault();
-  let data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
 }
 
 let addTask = function() {
-  if (!newTaskInput.value.length || !newTaskInput.value.trim().length){
-    alert("Name task can't null, please enter name task!");
-    return false;
-  }
+    if (!newTaskInput.value.length || !newTaskInput.value.trim().length) {
+        alert("Name task can't null, please enter name task!");
+        return false;
+    }
 
-  let task = createNewTaskElement(newTaskInput.value);
-  todo.append(task);
-  newTaskInput.value = "";
-  delete_task();
-  edit_task();
-  return true;
+    let task = createNewTaskElement(newTaskInput.value);
+    todo.append(task);
+    newTaskInput.value = "";
+    delete_task();
+    edit_task();
+    return true;
 }
 
 btnAdd.addEventListener("click", addTask);
 
 function delete_task() {
-  $(".delete-task").click(function() {
-    delete_id = parseInt(this.parentElement.parentElement.getAttribute('task_id'));
-    listTasks = listTasks.filter(value => value['taskId'] !== delete_id);
-    localStorage.setItem('task', JSON.stringify(listTasks));
-    this.parentElement.parentElement.remove();
-  });
+    $(".delete-task").click(function() {
+        delete_id = parseInt(this.parentElement.parentElement.getAttribute('task_id'));
+        listTasks = listTasks.filter(value => value['taskId'] !== delete_id);
+        localStorage.setItem('task', JSON.stringify(listTasks));
+        this.parentElement.parentElement.remove();
+    });
 }
 
-function submit_update(){
-  $('.submit-edit').click(function() {
-    let parent_element = this.parentElement.parentElement;
-    let value_text = parent_element.children[0].value;
-    edit_id = parseInt(parent_element.getAttribute('task_id'));
-    index_edit = listTasks.findIndex(value => value.taskId === edit_id);
-    listTasks[index_edit]['text'] = value_text;
-    localStorage.setItem('task', JSON.stringify(listTasks));
-    parent_element.children[0].remove();
-    span_element = document.createElement('span');
-    span_element.className = 'task-title';
-    span_element.innerHTML = value_text;
-    parent_element.prepend(span_element);
-    $('.submit-edit').remove();
-  });
+function submit_update() {
+    $('.submit-edit').click(function() {
+        let parent_element = this.parentElement.parentElement;
+        let value_text = parent_element.children[0].value;
+        edit_id = parseInt(parent_element.getAttribute('task_id'));
+        index_edit = listTasks.findIndex(value => value.taskId === edit_id);
+        listTasks[index_edit]['text'] = value_text;
+        localStorage.setItem('task', JSON.stringify(listTasks));
+        parent_element.children[0].remove();
+        span_element = document.createElement('span');
+        span_element.className = 'task-title';
+        span_element.innerHTML = value_text;
+        parent_element.prepend(span_elemebadge - successnt);
+        $('.submit-edit').remove();
+    });
 }
 
 function edit_task() {
-  $('.edit-task').click(function() {
+    $('.edit-task').click(function() {
 
-    let parentElementEdit = this.parentElement.parentElement;
-    let value_old = parentElementEdit.children[0].textContent;
-    let input_edit = document.createElement("input");
-    let submit_edit = document.createElement("div");
-    
-    input_edit.className = 'form-control edit-text'
-    input_edit.name = 'edit-text'
-    input_edit.value = value_old;
-    submit_edit.className = 'btn btn-success btn-sm mr-2 ml-2 submit-edit'
-    submit_edit.innerHTML = 'submit'
-    parentElementEdit.prepend(input_edit);
-    parentElementEdit.children[2].prepend(submit_edit);
-    parentElementEdit.children[1].remove();
-    
-    submit_update();
-  });
-}
+        let parentElementEdit = this.parentElement.parentElement;
+        let value_old = parentElementEdit.children[0].textContent;
+        let input_edit = document.createElement("input");
+        let submit_edit = document.createElement("div");
+
+        input_edit.className = 'form-control edit-text'
+        input_edit.name = 'edit-text'
+        input_edit.value = value_old;
+        submit_edit.className = 'btn btn-success btn-sm mr-2 ml-2 submit-edit'
+        submit_edit.innerHTML = 'submit'
+        parentElementEdit.prepend(input_edit);
+        parentElementEdit.children[2].prepend(submit_edit);
+        parentElementEdit.children[1].remove();
+
+        submit_update();
+    });
+};
